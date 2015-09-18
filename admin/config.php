beb729bf3d3803c42a54561aba7d05f8e7f9c2b3
naveen <naveen.nimbal@gmail.com>
2015-09-18 12:54:10 +0530

    Setting up OPen cart

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://nanakart.loc/admin/');
+define('HTTP_CATALOG', 'http://nanakart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://nanakart.loc/admin/');
+define('HTTPS_CATALOG', 'http://nanakart.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/oscom/admin/');
+define('DIR_SYSTEM', '/var/www/oscom/system/');
+define('DIR_LANGUAGE', '/var/www/oscom/admin/language/');
+define('DIR_TEMPLATE', '/var/www/oscom/admin/view/template/');
+define('DIR_CONFIG', '/var/www/oscom/system/config/');
+define('DIR_IMAGE', '/var/www/oscom/image/');
+define('DIR_CACHE', '/var/www/oscom/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/oscom/system/download/');
+define('DIR_UPLOAD', '/var/www/oscom/system/upload/');
+define('DIR_LOGS', '/var/www/oscom/system/logs/');
+define('DIR_MODIFICATION', '/var/www/oscom/system/modification/');
+define('DIR_CATALOG', '/var/www/oscom/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'nanakart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://nanakart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://nanakart.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/oscom/catalog/');
+define('DIR_SYSTEM', '/var/www/oscom/system/');
+define('DIR_LANGUAGE', '/var/www/oscom/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/oscom/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/oscom/system/config/');
+define('DIR_IMAGE', '/var/www/oscom/image/');
+define('DIR_CACHE', '/var/www/oscom/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/oscom/system/download/');
+define('DIR_UPLOAD', '/var/www/oscom/system/upload/');
+define('DIR_MODIFICATION', '/var/www/oscom/system/modification/');
+define('DIR_LOGS', '/var/www/oscom/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'nanakart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
